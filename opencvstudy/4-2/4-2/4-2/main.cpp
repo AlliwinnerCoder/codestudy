fb2cb9c250495c115ce4b53472f785c1802b04d5
unknown <bm00137@bmsoft.cn>
2018-03-22 18:43:06 +0800

    study

--- opencvstudy/4-2/4-2/4-2/main.cpp
+++ opencvstudy/4-2/4-2/4-2/main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 using namespace cv;
 
 void colorReduce(Mat& inputImage, Mat& outputImage, int div);
-
+void colorReduce1(Mat& inputImage, Mat& outputImage, int div);
+void colorReduce2(Mat& inputImage, Mat& outputImage, int div);
 int main()
 {
 	Mat srcImage = imread("1.jpg");
@@ -17,7 +18,7 @@ int main()
 
 	double time0 = static_cast<double>(getTickCount());
 
-	colorReduce(srcImage, dstImage, 32);
+	colorReduce2(srcImage, dstImage, 32);
 
 	time0 = ((double)getTickCount() - time0) / getTickFrequency();
 
@@ -31,5 +32,42 @@ void colorReduce(Mat& inputImage, Mat& outputImage, int div)
 {
 	outputImage = inputImage.clone();
 	int rowNumber = outputImage.rows;
-	int colNumber = outputImage.cols * outputI
+	int colNumber = outputImage.cols * outputImage.channels();
+	for (int i = 0; i < rowNumber; i++)
+	{
+		uchar * data = outputImage.ptr<uchar>(i);
+		for (int j = 0; j < colNumber; j++)
+		{
+			data[j] = data[j] / div*div + div / 2;
+		}
+	}
+}
+
+void colorReduce1(Mat& inputImage, Mat& outputImage, int div)
+{
+	outputImage = inputImage.clone();
+	Mat_<Vec3b>::iterator it = outputImage.begin<Vec3b>();
+	Mat_<Vec3b>::iterator itend = outputImage.end<Vec3b>();
+	for (;it != itend; ++it)
+	{
+		(*it)[0] = (*it)[0] / div*div + div / 2;
+		(*it)[1] = (*it)[0] / div*div + div / 2;
+		(*it)[2] = (*it)[2] / div*div + div / 2;
+	}
+}
+
+void colorReduce2(Mat& inputImage, Mat& outputImage, int div)
+{
+	outputImage = inputImage.clone();
+	int rowNumber = outputImage.rows;
+	int colNumber = outputImage.cols;
+	for (int i = 0; i < colNumber; i++)
+	{
+		for (int j = 0; j < colNumber; j++)
+		{
+			outputImage.at<Vec3b>(i, j)[0] = outputImage.at<Vec3b>(i, j)[0] / div*div + div / 2;
+			outputImage.at<Vec3b>(i, j)[1] = outputImage.at<Vec3b>(i, j)[1] / div*div + div / 2;
+			outputImage.at<Vec3b>(i, j)[2] = outputImage.at<Vec3b>(i, j)[2] / div*div + div / 2;
+		}
+	}
 }
