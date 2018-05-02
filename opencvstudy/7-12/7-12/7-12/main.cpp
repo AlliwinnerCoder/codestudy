50f8321621511342144227795b19a0ea1f295956
unknown <bm00137@bmsoft.cn>
2018-05-02 17:35:41 +0800

    study

--- opencvstudy/7-12/7-12/7-12/main.cpp
+++ opencvstudy/7-12/7-12/7-12/main.cpp
@@ -12,10 +12,10 @@ using namespace std;
 int main()
 {
 	Point2f srcTriangle[3];
-	Point2d dstTriangle[3];
+	Point2f dstTriangle[3];
 
 	Mat rotMat(2, 3, CV_32FC1);
-	Mat warpMat(2, 3, CV_32FC2);
+	Mat warpMat(2, 3, CV_32FC1);
 	Mat srcImage, dstImage_warp, dstImage_warp_rotate;
 
 	srcImage = imread("1.jpg", 1);
@@ -26,5 +26,21 @@ int main()
 
 	dstTriangle[0] = Point2f(static_cast<float>(srcImage.cols*0.0), static_cast<float>(srcImage.rows*0.33));
 	dstTriangle[1] = Point2f(static_cast<float>(srcImage.cols*0.65), static_cast<float>(srcImage.rows*0.35));
+	dstTriangle[2] = Point2f(static_cast<float>(srcImage.cols*0.15),static_cast<float>(srcImage.rows*0.6));
 
+	warpMat = getAffineTransform(srcTriangle, dstTriangle);
+	warpAffine(srcImage, dstImage_warp, warpMat, dstImage_warp.size());
+
+	Point center = Point(dstImage_warp.cols / 2, dstImage_warp.rows / 2);
+	double angle = -30.0;
+	double scale = 0.8;
+	rotMat = getRotationMatrix2D(center, angle, scale);
+	warpAffine(dstImage_warp, dstImage_warp_rotate, rotMat, dstImage_warp.size());
+
+	imshow(WINDOW_NAME1, srcImage);
+	imshow(WINDOW_NAME2, dstImage_warp);
+	imshow(WINDOW_NAME3, dstImage_warp_rotate);
+
+	waitKey(0);
+	return 0;
 }
