55e77b2aa5f790b8ee912b90bf26d45ab1a35ee0
unknown <bm00137@bmsoft.cn>
2018-05-20 13:56:36 +0800

    study

---
+++ opencvstudy/8-4/8-4/8-4/main.cpp
@@ -0,0 +1,33 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat image(600, 600, CV_8UC3);
+	RNG& rng = theRNG();
+	while (1)
+	{
+		int count = rng.uniform(3, 103);
+		vector<Point> points;
+		for (int i = 0; i < count; i++)
+		{
+			Point point;
+			point.x = rng.uniform(image.cols / 4, image.cols * 3 / 4);
+			point.y = rng.uniform(image.rows / 4, image.rows * 3 / 4);
+
+			points.push_back(point);
+		}
+
+		Point2f center;
+		float radius = 0;
+		minEnclosingCircle(Mat(points), center, radius);
+		image = Scalar::all(0);
+		for (int i = 0; i < count; i++)
+		{
+			circle(image, points[i], 3, Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)), FILLED, LINE_AA);
+		}
+		circle(image,center,cvRound(radius),Scalar(rng.uniform))
+	}
+}
